0d14e9931d3e272ffeff3fde1a6df9418a6f9ac6
Mario Metshein <metshein@gmail.com>
2026-03-19 10:14:17 +0200

    asdgsdfhg

--- index.php
+++ index.php
@@ -12,14 +12,14 @@
     <!-- menüü -->
       <nav class="navbar navbar-expand-lg bg-body-tertiary mb-4">
   <div class="container">
-    <a class="navbar-brand" href="#">Autorent</a>
+    <a class="navbar-brand" href="index.php">Autorent</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" href="#">Home</a>
+          <a class="nav-link active" href="index.php">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Link</a>
