e9546b789cb927ecccc34a45d05fa3d41c8fbea6
Mario Metshein <metshein@gmail.com>
2026-03-19 11:17:21 +0200

    wg

--- single_car.php
+++ single_car.php
@@ -1,44 +1,6 @@
 <?php include('config.php'); ?>
+<?php include('header.php'); ?>
 
-<!doctype html>
-<html lang="et">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Autorent</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-  </head>
-  <body>
-    <!-- menüü -->
-      <nav class="navbar navbar-expand-lg bg-body-tertiary mb-4">
-  <div class="container">
-    <a class="navbar-brand" href="#">Autorent</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search" method="get" action="index.php">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="otsi">
-        <button class="btn btn-outline-success" type="submit">Otsi</button>
-      </form>
-    </div>
-  </div>
-</nav>
-    <!-- /menüü -->
 <!-- sisu -->
 <div class="container">
 
@@ -50,13 +12,18 @@
     $paring = "SELECT * FROM cars WHERE id=".$id."";
     $valjund = mysqli_query($yhendus, $paring);
     $rida = mysqli_fetch_assoc($valjund);
-    // print_r($rida);
+     print_r($rida);
 ?>
         <div class="col">
             <h1><?php echo $rida["mark"]; ?> <?php echo $rida["model"]; ?></h1>
             <p>Mootor:  <?php echo $rida["engine"]; ?></p>
             <p>Kütus:  <?php echo $rida["fuel"]; ?></p>
-            <p>Hind:  <?php echo $rida["price"]; ?></p>
+            <p>Aasta:  <?= $rida["year"]; ?></p>
+            <p>Staatus:  <?php echo $rida["status"]; ?></p>
+            <p>Käigukast:  <?php echo $rida["transmission"]; ?></p>
+            <p>Istmed:  <?php echo $rida["seats"]; ?></p>
+            <p class="fs-5">Hind:  <?php echo $rida["price"]; ?></p>
+            <a href="#" class="btn btn-dark w-100">Rendi auto</a>
         </div>
         <div class="col">
             <img src="https://loremflickr.com/800/500/<?php echo str_replace(" ","", $rida["mark"]); ?>" class="card-img-top img-fluid" alt="<?php echo $rida["mark"]; ?>">
