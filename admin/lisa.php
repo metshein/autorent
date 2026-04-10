a9ee5c2dd83f85bbe4962779f0521bd92ca54252
Mario Metshein <metshein@gmail.com>
2026-04-10 09:20:30 +0300

    Add default values and additional fields to car addition form

--- admin/lisa.php
+++ admin/lisa.php
@@ -8,18 +8,27 @@
         <div class="row g-4">
             <div class="col-sm-6">
                 <label for="mark" class="form-label">Mark</label>
-                <input type="text" class="form-control" id="mark" name="mark">
+                <input type="text" class="form-control" id="mark" name="mark" value="test">
                 <label for="model" class="form-label">Model</label>
-                <input type="text" class="form-control" id="model" name="model">
+                <input type="text" class="form-control" id="model" name="model" value="test">
                 <label for="engine" class="form-label">Mootor</label>
-                <input type="text" class="form-control" id="engine" name="engine">
+                <input type="text" class="form-control" id="engine" name="engine" value="test">
                 <label for="fuel" class="form-label">Kütus</label>
-                <input type="text" class="form-control" id="fuel" name="fuel">
+                <input type="text" class="form-control" id="fuel" name="fuel" value="test">
                 <label for="price" class="form-label">Hind</label>
-                <input type="number" class="form-control" id="price" name="price">
+                <input type="number" class="form-control" id="price" name="price" value="123">
             </div>
             <div class="col-sm-6">
-
+                <label for="year" class="form-label">Aasta</label>
+                <input type="number" class="form-control" id="year" name="year" value="2000">
+                <label for="transmission" class="form-label">Käigukast</label>
+                <input type="text" class="form-control" id="transmission" name="transmission" value="automaat">
+                <label for="seats" class="form-label">Istmete arv</label>
+                <input type="number" class="form-control" id="seats" name="seats" value="5">
+                <label for="description" class="form-label">Muu info</label>
+                <input type="text" class="form-control" id="description" name="description" value="test">
+                <label for="status" class="form-label">Olek</label>
+                <input type="text" class="form-control" id="status" name="status" value="vaba">
             </div>
             <input type="submit" value="Salvesta" class="btn btn-success">
         </div>
