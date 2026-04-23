dcbfa0455ef790a6d3117f5464aaef4abf264183
Mario Metshein <metshein@gmail.com>
2026-04-23 14:21:05 +0300

    Add login and logout functionality with user authentication

---
+++ admin/login.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  </head>
+  <body>
+<?php
+    $msg = "";
+
+    if (!empty($_POST)) {
+        $uname = $_POST['user'];
+        $password = $_POST['password'];
+        $hash = '$2y$10$GShDgMs0q43XaQmeWGq0OekAo8RR2wsQWhAgXd4SlYeVwr0zoheaC';
+
+        if ($uname=="admin" && password_verify($password, $hash)) {
+            echo "tere admin";
+        }else{
+            $msg = "kasutaja vale";
+        }
+
+    }
+
+?>
+    <div class="container">
+        <div class="row pt-4 mt-4">
+            <div class="col-sm-4"></div>
+            <div class="col-sm-4">
+                <form method="post" action="login.php" autocomplete="off">
+                    <div class="mb-3">
+                        <label for="u" class="form-label">Username</label>
+                        <input name="user" type="text" class="form-control" id="u">
+                    </div>
+                    <div class="mb-3">
+                        <label for="p" class="form-label">Password</label>
+                        <input name="password" type="password" class="form-control" id="p" >
+                    </div>
+                    <button type="submit" class="btn btn-primary">Logi sisse</button>
+                </form>
+                <?php echo $msg; ?>
+            </div>
+            <div class="col-sm-4"></div>
+        </div>
+    </div>
+  
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </body>
+</html>
